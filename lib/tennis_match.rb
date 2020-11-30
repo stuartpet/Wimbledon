a8f11ee3abb2f5e5dbcde2e4f55744269509f1fe
stuartpet <stuart.pethurst@gmail.com>
2020-11-30 14:20:24 +0000

    refactor and added methods to allow 2nd player to win points

--- lib/tennis_match.rb
+++ lib/tennis_match.rb
@@ -22,7 +22,7 @@ class TennisMatch
 
   def ready
     puts "Hi #{player_1} and #{player_2}, get ready"
-    serve
+    player_1_serve
   end
 
   def rules
@@ -38,29 +38,65 @@ class TennisMatch
     HEREDOC
   end
 
-  def serve
-    puts "#{player_1} please choose left, right or middle"
+  def player_1_serve
+    puts "#{player_1} please serve using left, right or middle"
     p1_shot = gets.chomp
-    puts "#{player_2} return with left, right or middle"
-    p2_shot = gets.chomp
-    player_shot(p1_shot, p2_shot)
+    player_2_shot(p1_shot)
   end
 
-  def player_shot(p1_shot, p2_shot)
-    if p1_shot == p2_shot
-      serve
-    elsif p1_shot != p2_shot
-      score_keeper
+  def player_1_shot(p2_shot)
+    puts "#{player_1} please move player using left, right or middle"
+    p1_move = gets.chomp
+    puts "#{player_1} please return shot using left, right or middle"
+    p1_shot = gets.chomp
+    player_1_shot_calculation(p1_shot, p1_move, p2_shot)
+  end
+
+  def player_2_shot(p1_shot)
+    puts "#{player_2} please move player using left, right or middle"
+    p2_move = gets.chomp
+    puts "#{player_2} please return shot using left, right or middle"
+    p2_shot = gets.chomp
+    player_2_shot_calculation(p2_shot, p2_move, p1_shot)
+  end
+
+  def player_2_shot_calculation(p2_shot, p2_move, p1_shot)
+    if p2_move == p1_shot
+      player_1_shot(p2_shot)
+    elsif p2_move != p1_shot
+      p1_score_keeper
     end
   end
 
-  def score_keeper
-    if @p1_score > 40
+  def player_1_shot_calculation(p1_shot, p1_move, p2_shot)
+    if p1_move == p2_shot
+      player_2_shot(p1_shot)
+    elsif p1_move != p2_shot
+      p2_score_keeper
+    end
+  end
+
+  def p1_score_keeper
+    if @p1_score >= 40
       puts "Congratulations #{player_1} you won the game"
-      elsif @p1_score != 30
-      @p1_score += 15
-    elsif
+    elsif @p1_score == 30
       @p1_score += 10
+      puts "#{player_2} you missed, #{player_1}'s score is now '#{p1_score}"
+    else
+      @p1_score += 15
+      puts "#{player_2} you missed, #{player_1}'s score is now '#{p1_score}"
+    end
+  end
+
+  def p2_score_keeper
+    if @p2_score >= 40
+      puts "Congratulations #{player_2} you won the game"
+    elsif @p2_score == 30
+      @p2_score += 10
+      puts "#{player_1} you missed, #{player_2}'s score is now '#{p2_score}"
+    else
+      @p2_score += 15
+      puts "#{player_1} you missed, #{player_2}'s score is now '#{p2_score}"
     end
   end
 end
--- spec/tennis_match_spec.rb
+++ spec/tennis_match_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 
 RSpec.describe TennisMatch do
 
-  subject { described_class.new(player_1: player_1, p1_score: p1_score, p2_score: p2_score) }
+  subject { described_class.new(player_1: player_1, player_2: player_2, p1_score: p1_score, p2_score: p2_score) }
 
   let(:player_1) { 'Nadal' }
   let(:player_2) { 'Rafa' }
@@ -26,33 +26,42 @@ RSpec.describe TennisMatch do
 
   context 'when a player starts a service game' do
     it 'it will ask them to choose left, right or middle' do
-      expect { subject.serve }.to output.to_stdout
+      expect { subject.player_1_serve }.to output.to_stdout
     end
   end
 
-  context 'when a player selects an action' do
-    let(:p1_shot) { 'left' }
+  context 'when a player moves in the wrong direction to the shot' do
+    let(:p1_move) { 'left' }
     let(:p2_shot) { 'right' }
 
-    it 'checks for an equal opponent\'s reaction' do
-      expect { subject.player_shot(p1_shot, p2_shot) }.to change { subject.p1_score }.by(15)
+    it 'adds the correct points value to that players score' do
+      expect { subject.player_1_shot(p2_shot) }.to change { subject.p2_score }.by(15)
+    end
+  end
+
+  context 'when a player moves in the right direction as a shot' do
+    let(:p1_move) { 'right' }
+    let(:p2_shot) { 'right' }
+
+    it 'does not add any points to the players score' do
+      expect { subject.player_1_shot(p2_shot) }.not_to(change { subject.p1_score })
     end
   end
 
 
   context 'when a player has 30 points and scores another point' do
-    let(:p1_score) {30}
+    let(:p1_score) { 30 }
 
     it 'increase the score by 10' do
-      expect { subject.score_keeper }.to change { subject.p1_score }.by(10)
+      expect { subject.p1_score_keeper }.to change { subject.p1_score }.by(10)
     end
   end
 
   context 'when a player has 40 points and wins the next point' do
-    let(:p1_score) { 41 }
+    let(:p1_score) { 40 }
 
     it 'prints a message to inform the player they have won' do
-      expect { subject.score_keeper }.to output("Congratulations #{player_1} you won the game\n").to_stdout
+      expect { subject.p1_score_keeper }.to output("Congratulations #{player_1} you won the game\n").to_stdout
     end
   end
 
